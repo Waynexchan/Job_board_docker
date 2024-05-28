6cf7f7933598e48b1215ca4ae1a64b50f4339db5
wayne chan <waynestock666@gmail.com>
2024-05-28 21:10:42 +0100

    update

--- config/settings.py
+++ config/settings.py
@@ -118,15 +118,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
